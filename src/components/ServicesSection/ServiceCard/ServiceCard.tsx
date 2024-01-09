f8b00f242ca99864a9dcbc4bce3b9a2504d6ffb1
LordCail1 <rami.batnigi@gmail.com>
2024-01-09 15:38:32 -0500

    got effect to work

--- src/components/ServicesSection/ServiceCard/ServiceCard.tsx
+++ src/components/ServicesSection/ServiceCard/ServiceCard.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { motion } from "framer-motion"
+import { Variants, motion } from "framer-motion"
+import { useState } from "react"
 
 type Props = {
 	icon: string
@@ -11,19 +12,79 @@ type Props = {
  * Those are the cards that contain the different services
  */
 export default function ServiceCard({ description, icon, title }: Props) {
+	const [isHovered, setIsHovered] = useState(false)
+
+	const horizontalVariant: Variants = {
+		hidden: {
+			width: 0,
+		},
+		visible: {
+			width: "100%",
+			transition: {
+				duration: 0.5,
+				ease: "easeInOut",
+			},
+		},
+	}
+
+	const verticalVariant: Variants = {
+		hidden: {
+			height: 0,
+		},
+		visible: {
+			height: "100%",
+			transition: {
+				duration: 0.5,
+				ease: "easeInOut",
+			},
+		},
+	}
+
 	return (
-		<article
+		<motion.article
+			onHoverStart={() => {
+				console.log(isHovered)
+				setIsHovered(true)
+			}}
+			onHoverEnd={() => {
+				console.log(isHovered)
+				setIsHovered(false)
+			}}
 			className={
-				"flex-grow basis-full cursor-pointer flex-col items-center justify-center px-40 lg:px-72 py-40 transition hover:bg-blue_island_hover xl:basis-1/2 xl:p-40"
+				"group relative flex-grow basis-full cursor-pointer flex-col items-center justify-center px-40 py-40 transition hover:bg-blue_island_hover lg:px-72 xl:basis-1/2 xl:p-40"
 			}
 		>
 			<figure className="pt-6">
-				<h1 className="pb-20 text-4xl text-white group-hover:text-black">{title}</h1>
+				<h1 className="pb-20 text-4xl text-american_silver group-hover:text-white">{title}</h1>
 			</figure>
-			<div className="text-american_silver group-hover:text-black">
+			<div className="text-american_silver">
 				<hr className="w-1/2" />
 				<p className="py-20 text-lg ">{description}</p>
 			</div>
-		</article>
+			<motion.div
+				animate={isHovered ? "visible" : "hidden"}
+				className="absolute bottom-0 left-0 h-[1px] bg-mint_morning"
+				id="bottom"
+				variants={horizontalVariant}
+			/>
+			<motion.div
+				animate={isHovered ? "visible" : "hidden"}
+				className="absolute bottom-0 right-0 w-[1px] bg-mint_morning"
+				id="right"
+				variants={verticalVariant}
+			/>
+			<motion.div
+				id="top"
+				animate={isHovered ? "visible" : "hidden"}
+				className="absolute right-0 top-0 h-[1px] bg-mint_morning"
+				variants={horizontalVariant}
+			/>
+			<motion.div
+				id="left"
+				animate={isHovered ? "visible" : "hidden"}
+				className="absolute left-0 top-0 w-[1px] bg-mint_morning"
+				variants={verticalVariant}
+			/>
+		</motion.article>
 	)
 }
