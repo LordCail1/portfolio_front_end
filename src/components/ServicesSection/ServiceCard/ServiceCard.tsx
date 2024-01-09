2ea10575bb797fb673965a79a2ce387d0935b6a8
LordCail1 <rami.batnigi@gmail.com>
2024-01-09 14:07:35 -0500

    made serviceCard component client component

--- src/components/ServicesSection/ServiceCard/ServiceCard.tsx
+++ src/components/ServicesSection/ServiceCard/ServiceCard.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { motion } from "framer-motion"
+
 type Props = {
 	icon: string
 	title: string
@@ -11,16 +14,14 @@ export default function ServiceCard({ description, icon, title }: Props) {
 	return (
 		<article
 			className={
-				"flex-grow basis-1/2 cursor-pointer flex-col items-center justify-center p-40 transition hover:bg-blue_island_hover"
+				"flex-grow basis-full cursor-pointer flex-col items-center justify-center px-40 lg:px-72 py-40 transition hover:bg-blue_island_hover xl:basis-1/2 xl:p-40"
 			}
 		>
 			<figure className="pt-6">
-				<h1 className="pb-20 text-4xl font-bold text-american_silver group-hover:text-black">
-					{title}
-				</h1>
+				<h1 className="pb-20 text-4xl text-white group-hover:text-black">{title}</h1>
 			</figure>
 			<div className="text-american_silver group-hover:text-black">
-				<hr className="w-1/2 group-hover:border-black" />
+				<hr className="w-1/2" />
 				<p className="py-20 text-lg ">{description}</p>
 			</div>
 		</article>
